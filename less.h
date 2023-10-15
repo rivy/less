3352ce782e4c7805eee4c1120256cb56ce8eb073
Roy Ivy III <rivy.dev@gmail.com>
2023-10-15 10:29:19 -0500

    wip ~ fix gcc redefined without dllimport property

--- less.h
+++ less.h
@@ -638,6 +638,6 @@ void inttoa(int, char*, int);
 int lstrtoi(char*, char**, int);
 POSITION lstrtopos(char*, char**, int);
 unsigned long lstrtoul(char*, char**, int);
-#if MSDOS_COMPILER==WIN32C
+#if MSDOS_COMPILER==WIN32C && !defined(__GNUC__)
 int pclose(FILE*);
 #endif
